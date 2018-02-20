b07efd08310da8bd75284b49f0ec5e5e71a4e991
Katerina <katerina.loschinina@ft.com>
2018-02-20 10:36:04 +0000

    add event listener to alert-message-success-demo button that opens corrsponding alert

--- demos/src/demo.js
+++ demos/src/demo.js
@@ -1,9 +1,26 @@
 /*global require*/
 import './../../main.js';
+import Message from './../../src/js/message';
 
-function initDemos() {
+const initDemos = () => {
 	document.addEventListener('DOMContentLoaded', function() {
 		document.dispatchEvent(new CustomEvent('o.DOMContentLoaded'));
+
+		document.getElementById('alert-message-success-demo').addEventListener('click', () => {
+			new Message(null, {
+				theme: 'success',
+				content: {
+					highlight: 'Something went right.',
+					detail: 'The quick brown fox jumped over the lazy dogs'
+				},
+				link: {
+					text: 'Relevant Link'
+				},
+				button: {
+					text: 'Button'
+				}
+			});
+		});
 	});
 }
 
