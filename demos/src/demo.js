20c11f78daf43ec8dce487189ff19b1088fd6362
Katerina <katerina.loschinina@ft.com>
2018-02-20 10:53:33 +0000

    add check if the button exists, so no errors on other pages

--- demos/src/demo.js
+++ demos/src/demo.js
@@ -6,70 +6,78 @@ const initDemos = () => {
 	document.addEventListener('DOMContentLoaded', function() {
 		document.dispatchEvent(new CustomEvent('o.DOMContentLoaded'));
 
-		document.getElementById('alert-message-success-demo').addEventListener('click', () => {
-			new Message(null, {
-				theme: 'success',
-				content: {
-					highlight: 'Something went right.',
-					detail: 'The quick brown fox jumped over the lazy dogs'
-				},
-				link: {
-					text: 'Relevant Link'
-				},
-				button: {
-					text: 'Button'
-				}
+		if(document.getElementById('alert-message-success-demo')) {
+			document.getElementById('alert-message-success-demo').addEventListener('click', () => {
+				new Message(null, {
+					theme: 'success',
+					content: {
+						highlight: 'Something went right.',
+						detail: 'The quick brown fox jumped over the lazy dogs'
+					},
+					link: {
+						text: 'Relevant Link'
+					},
+					button: {
+						text: 'Button'
+					}
+				});
 			});
-		});
+		}
 
-		document.getElementById('alert-message-neutral-demo').addEventListener('click', () => {
-			new Message(null, {
-				theme: 'neutral',
-				content: {
-					highlight: 'Meh.',
-					detail: 'The quick brown fox jumped over the lazy dogs'
-				},
-				link: {
-					text: 'Relevant Link'
-				},
-				button: {
-					text: 'Button'
-				}
+		if(document.getElementById('alert-message-neutral-demo')) {
+			document.getElementById('alert-message-neutral-demo').addEventListener('click', () => {
+				new Message(null, {
+					theme: 'neutral',
+					content: {
+						highlight: 'Meh.',
+						detail: 'The quick brown fox jumped over the lazy dogs'
+					},
+					link: {
+						text: 'Relevant Link'
+					},
+					button: {
+						text: 'Button'
+					}
+				});
 			});
-		});
+		}
 
-		document.getElementById('alert-message-neutral-bleed-demo').addEventListener('click', () => {
-			new Message(null, {
-				theme: 'neutral',
-				bleed: 'true',
-				content: {
-					highlight: 'Meh.',
-					detail: 'The quick brown fox jumped over the lazy dogs'
-				},
-				link: {
-					text: 'Relevant Link'
-				},
-				button: {
-					text: 'Button'
-				}
+		if(document.getElementById('alert-message-neutral-bleed-demo')) {
+			document.getElementById('alert-message-neutral-bleed-demo').addEventListener('click', () => {
+				new Message(null, {
+					theme: 'neutral',
+					bleed: 'true',
+					content: {
+						highlight: 'Meh.',
+						detail: 'The quick brown fox jumped over the lazy dogs'
+					},
+					link: {
+						text: 'Relevant Link'
+					},
+					button: {
+						text: 'Button'
+					}
+				});
 			});
-		});
+		}
 
-		document.getElementById('alert-message-failure-demo').addEventListener('click', () => {
-			new Message(null, {
-				theme: 'error',
-				content: {
-					highlight: 'Something went wrong.',
-					detail: 'The quick brown fox jumped over the lazy dogs'
-				},
-				link: {
-					text: 'Relevant Link'
-				},
-				button: {
-					text: 'Button'
-				}
+		if(document.getElementById('alert-message-failure-demo')) {
+			document.getElementById('alert-message-failure-demo').addEventListener('click', () => {
+				new Message(null, {
+					theme: 'error',
+					content: {
+						highlight: 'Something went wrong.',
+						detail: 'The quick brown fox jumped over the lazy dogs'
+					},
+					link: {
+						text: 'Relevant Link'
+					},
+					button: {
+						text: 'Button'
+					}
+				});
 			});
-		});
+		}
 	});
 }
 
