b95a87ab024c1e0404baba2fec30aeaf95d19531
Katerina <katerina.loschinina@ft.com>
2018-02-21 18:35:23 +0000

    change constructor to reflect the logic

--- src/js/construct-element.js
+++ src/js/construct-element.js
@@ -11,8 +11,13 @@ export default {
 		alertMessageEl.setAttribute('data-o-component', 'o-message');
 		alertMessageEl.classList.add(opts.messageClass, `${opts.messageClass}--closed`);
 
-		// bleed message element
-		alertMessageEl.classList.add(`${opts.bleed ? opts.typeClass + '--bleed' : opts.typeClass}`);
+		if (opts.bleed) {
+			alertMessageEl.classList.add(`${opts.typeClass}--bleed`);
+		} else if (opts.inline) {
+			alertMessageEl.classList.add(`${opts.typeClass}--inline`);
+		} else {
+			alertMessageEl.classList.add(`${opts.typeClass}`);
+		}
 
 		if (!opts.theme) {
 			throwError("Alert type messages require a theme. The options are 'success', 'error', or 'neutral'");
@@ -23,17 +28,26 @@ export default {
 		let contentHTML;
 		if (opts.content.detail) {
 			contentHTML = `
-				<div class="${opts.messageClass}__content--header">
-					<span class="${opts.messageClass}__content--highlight">${opts.content.highlight}</span>
-					<p class="${opts.messageClass}__content--detail">${opts.content.detail}</p>
+				<div class="${opts.messageClass}__content">
+					<p class="${opts.messageClass}__content--detail"><span class="${opts.messageClass}__content--highlight">${opts.content.highlight}</span>${opts.content.detail}</p>`;
+		} else {
+			contentHTML = `
+				<div class="${opts.messageClass}__content">
+					<span class="${opts.messageClass}__content--highlight">${opts.content.highlight}</span>`;
+		}
+
+		if (opts.inline && opts.content.additionalInfo) {
+			contentHTML += `<p class="${opts.messageClass}__content--additional-info">${opts.content.additionalInfo}</p>
 				</div>
 			`;
 		} else {
-			contentHTML = `
-				<div class="${opts.messageClass}__content--header">
-					<span class="${opts.messageClass}__content--highlight">${opts.content.highlight}</span>
+			contentHTML += `
 				</div>
 			`;
+		}
+
+		if (opts.inline) {
+			opts.close = false;
 		}
 
 		let primaryActionHTML;
@@ -49,6 +63,8 @@ export default {
 		let closeButton;
 		if (opts.close) {
 			closeButton = `<a href="#void" class="${opts.messageClass}__close" role="button" aria-label='Close' title='Close'></a>`;
+		} else {
+			closeButton = '';
 		}
 
 		alertMessageEl.innerHTML = `
@@ -61,58 +77,5 @@ export default {
 		`;
 
 		return alertMessageEl;
-	},
-
-	inlineAlertMessage: (opts) => {
-		const inlineAlertMessage = document.createElement('div');
-		inlineAlertMessage.setAttribute('data-o-component', 'o-message');
-		inlineAlertMessage.classList.add(opts.messageClass, `${opts.messageClass}--inline-alert`);
-
-		if (!opts.theme) {
-			throwError("Alert type messages require a theme. The options are 'success', 'error', or 'neutral'");
-		} else {
-			inlineAlertMessage.classList.add(`${opts.typeClass}-${opts.theme}`);
-		}
-
-		let contentHTML;
-		if (opts.content.detail) {
-			contentHTML = `
-			<div class="${opts.messageClass}__content--header">
-				<p class="${opts.messageClass}__content--detail"><span class="${opts.messageClass}__content--highlight">${opts.content.highlight}</span> ${opts.content.detail}</p>
-			</div>
-			`;
-		} else {
-			contentHTML = `
-			<div class="${opts.messageClass}__content--header">
-				<p class="${opts.messageClass}__content--detail"><span class="${opts.messageClass}__content--highlight">${opts.content.highlight}</span></p>
-			</div>
-			`;
-		}
-
-		let contentActionHTML;
-		if (opts.content.action) {
-			contentActionHTML = `<p class="${opts.messageClass}__content--action">${opts.content.action}</p>`;
-		}
-
-		let primaryActionHTML;
-		if (opts.button.text) {
-			primaryActionHTML = `<a href="${opts.button.url}" class="${opts.messageClass}__button ${opts.messageClass}__action--primary">${opts.button.text}</a>`;
-		}
-
-		let secondaryActionHTML;
-		if (opts.link.text) {
-			secondaryActionHTML = `<a href="${opts.link.url}" class="${opts.messageClass}__link ${opts.messageClass}__action--secondary">${opts.link.text}</a>`;
-		}
-
-		inlineAlertMessage.innerHTML = `
-			<div class="${opts.messageClass}__container">
-				${contentHTML}
-				${contentActionHTML}
-				${primaryActionHTML}
-				${secondaryActionHTML}
-			</div>
-		`;
-
-		return inlineAlertMessage;
 	}
 };
