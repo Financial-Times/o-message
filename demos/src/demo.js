eb652c0cc16f8ecea9ead3290454a1635a7a5c6d
Gabrielle von Koss <gabivk@gmail.com>
2018-03-05 11:33:31 +0000

    more sass update

--- demos/src/demo.js
+++ demos/src/demo.js
@@ -2,6 +2,29 @@
 import './../../main.js';
 import Message from './../../src/js/message';
 
+let options = {
+	content: {
+		highlight: 'Highlight!',
+		detail: 'Details about the message go here.'
+	},
+	link: {
+		text: 'Relevant Link'
+	},
+	button: {
+		text: 'Button'
+	}
+}
+
+let inlineOptions = Object.assign({}, options, {
+	inline: true,
+	parentElement: '.demo-inline',
+	content: {
+		highlight: 'Highlight!',
+		detail: 'Details about the message go here.',
+		additionalInfo: 'If there is more to say about this, please feel free to say it here.'
+	}
+});
+
 const deleteElementsByClassName  = (className) => {
 	const elementsWithThisClass = document.getElementsByClassName(className);
 	while (elementsWithThisClass[0]) {
@@ -9,45 +32,22 @@ const deleteElementsByClassName  = (className) => {
 	}
 }
 
-const setUpDemo = (id, element, opts, variants) => {
+const setUpDemo = (id, opts, variants) => {
 	document.getElementById(id).addEventListener('click', () => {
 		deleteElementsByClassName('o-message');
-		return new Message(element, Object.assign({}, opts, variants));
+		return new Message(null, Object.assign({}, opts, variants));
 	});
 }
 
 const initDemos = () => {
-	let options = {
-		content: {
-			highlight: 'Highlight!',
-			detail: 'Details about the message go here.'
-		},
-		link: {
-			text: 'Relevant Link'
-		},
-		button: {
-			text: 'Button'
-		}
-	}
+	setUpDemo('alert-success', options, {theme: 'success'});
+	setUpDemo('alert-neutral', options, {theme: 'neutral'});
+	setUpDemo('alert-error', options, {theme: 'error'});
+	setUpDemo('alert-error-bleed', options, {theme: 'error', bleed: true});
 
-	let inlineOptions = Object.assign({}, options, {
-		inline: true,
-		parentElement: '.demo-inline-message',
-		content: {
-			highlight: 'Highlight!',
-			detail: 'Details about the message go here.',
-			additionalInfo: 'If there is more to say about this, please feel free to say it here.'
-		}
-	});
-
-	setUpDemo('alert-success', null, options, {theme: 'success'});
-	setUpDemo('alert-neutral', null, options, {theme: 'neutral'});
-	setUpDemo('alert-error', null, options, {theme: 'error'});
-	setUpDemo('alert-error-bleed', null, options, {theme: 'error', bleed: true});
-
-	setUpDemo('inline-success', null, inlineOptions, {theme: 'success'});
-	setUpDemo('inline-neutral', null, inlineOptions, {theme: 'neutral'});
-	setUpDemo('inline-error', null, inlineOptions, {theme: 'error'});
+	setUpDemo('inline-success', inlineOptions, {theme: 'success'});
+	setUpDemo('inline-neutral', inlineOptions, {theme: 'neutral'});
+	setUpDemo('inline-error', inlineOptions, {theme: 'error'});
 }
 
 document.addEventListener('DOMContentLoaded', () => {
